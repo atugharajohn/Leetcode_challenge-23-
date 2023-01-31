4312a433344b88a56b341a7cb4cb1662dfb1ada1
atugharajohn <atugharajohn@gmail.com>
2023-01-31 23:53:59 +0530

    updated read me and added code to update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Leetcode-Solutions
+# Leetcode Solutions
+
+<details close> 
+	<summary>Jan Challenge:</summary>
+<ol><li>
+
+[290. Word Pattern.py](https%3A//github.com/ThisIsSakshi/Leetcode-Solutions/blob/main//Jan%20Challenge/290.%20Word%20Pattern.py) 
+</li>
+</ol></details>
---
+++ write_to_readme.py
@@ -0,0 +1,18 @@
+import os
+from urllib import parse
+
+l = list(set(os.listdir("."))-set(['write_to_readme.py', 'README.md', '.git','.gitignore','.DS_Store']))
+print(l)
+common_path = 'https://github.com/ThisIsSakshi/Leetcode-Solutions/blob/main/'
+s='# Leetcode Solutions'
+for month in l:
+    s+='\n\n<details close> \n'
+    s+='\t<summary>'+month+':</summary>\n<ol>'
+    for question in os.listdir('./'+month):
+        code_path = parse.quote(common_path+'/'+month+'/'+question)
+        s+= '<li>\n\n['+question+']('+code_path+') \n</li>'
+    s+='\n</ol></details>'
+
+file = open('README.md','w')
+file.write(s)
+file.close() 
