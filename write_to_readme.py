dca533a7e98134d586338fcf14767d0e5ed55003
atugharajohn <atugharajohn@gmail.com>
2023-03-18 20:31:40 +0530

    modified readme writer

--- write_to_readme.py
+++ write_to_readme.py
@@ -4,14 +4,16 @@ from urllib import parse
 
 l = [n for n in os.listdir(".") if '.' not in n]
 l = sorted(l,key = lambda x: os.path.getmtime('./'+x))
-month_bullet= {'Jan Challenge':'💝','Feb Challenge':'🧡','Mar Challenge':'💚' ,'Apr Challenge':'💙', 'May Challenge':'💛', 'Jun Challenge':'💜','Jul Challenge':'🤎','Aug Challenge':'🖤','Sep Challenge':'🤍','Oct Challenge':'💕','Nov Challenge':'💖','Dec Challenge':'❤️‍🩹'}
+# month_bullet= {'Jan Challenge':'💝','Feb Challenge':'🧡','Mar Challenge':'💚' ,'Apr Challenge':'💙', 'May Challenge':'💛', 'Jun Challenge':'💜','Jul Challenge':'🤎','Aug Challenge':'🖤','Sep Challenge':'🤍','Oct Challenge':'💕','Nov Challenge':'💖','Dec Challenge':'❤️‍🩹'}
+month_count=1
 
+month_gif = lambda mon,wid: '<img src = "https://leetcode.com/static/images/badges/2023/gif/2023-'+str(mon).zfill(2)+'.gif" width="'+str(wid)+'"/>'
 common_path = 'https://github.com/ThisIsSakshi/Leetcode-Solutions/blob/main/'
 s='# Leetcode Solutions'
 for month in l:
     sno=1
     s+='\n\n<details close> \n'
-    s+='\t<summary style="font-size:25px;">'+month+':</summary>'
+    s+='\t<summary>'+month_gif(month_count,60)+month+':</summary>'
 
     all_questions = os.listdir('./'+month)
     all_questions = sorted(all_questions,key = lambda x: os.path.getmtime('./'+month+'/'+x)) 
@@ -24,9 +26,10 @@ for month in l:
             new_question_name = str(sno)+': '+question
             os.rename(file_path+question,file_path+new_question_name)
         code_path = common_path+parse.quote(month+'/'+new_question_name)
-        s+= '\n\n'+month_bullet[month]+' ['+new_question_name+']('+code_path+') \n'
+        s+= '\n\n'+month_gif(month_count,15)+' ['+new_question_name+']('+code_path+') \n'
         sno+=1
     s+='\n</details>'
+    month_count+=1
 
 file = open('README.md','w')
 file.write(s)
